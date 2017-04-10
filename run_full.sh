ef8a5b55e5882c5e2288202ed47cfca94fa1aa6f
michael vogelsang <mjvoge02@louisville.edu>
2017-04-10 02:55:54 -0400

    fix script

--- run_full.sh
+++ run_full.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -x
 cd ./clean_data && rm *.csv
 cd ..
 rm ./LouData.db
 rm ./dbdump.bak
 python ./1_CleanData.py
 python ./2_LoadDataToDbAndFilter.py
-sqlite3 .dump > dbdump.bak
+sqlite3 ./LouData.db .dump > dbdump.bak
