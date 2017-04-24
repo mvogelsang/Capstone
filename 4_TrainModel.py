5cd64e5d0484a95da1027ed0bd013678a2417fa4
michael vogelsang <mjvoge02@louisville.edu>
2017-04-23 20:33:05 -0400

    get basic training working

--- 4_TrainModel.py
+++ 4_TrainModel.py
@@ -3,13 +3,13 @@ import os
 import sqlite3
 import datetime
 import sqlQueries
+import time
 import sklearn
-from sklearn import preprocessing
+from sklearn import preprocessing, metrics, model_selection, linear_model
 import pyglmnet
 import numpy
 import math
 import pickle
-import sklearn.metrics as skm
 
 # note, for convenience of writing many separate query functions
 # the connection is defined globally
@@ -39,16 +39,29 @@ def getStandardScaler(trainingInput):
     scaler.fit(trainingInput)
     return scaler
 
-def getTestPredictions(testInput, analysisTool):
-    predictions = []
-    for sample in testInput:
-        prediction = analysisTool.predict(sample)
-        prediction = numpy.mean(prediction)
-        predictions.append(prediction)
+def getTestPredictions(testInput, analysisTool, predictsAnArray):
+    if predictsAnArray:
+        predictions = []
+        for datapoint in testInput:
+            predictions.append(numpy.mean(analysisTool.predict(datapoint)))
+    else:
+        predictions = analysisTool.predict(testInput)
 
     return predictions
 
-def fitEstimator(tool, )
+def fitEstimator(tool, paramDict, trainingInput, trainingOutput):
+
+    # track time taken to run search
+    start = time.time()
+    # newTool = model_selection.GridSearchCV(estimator=tool, param_grid=paramDict, n_jobs=2, pre_dispatch=4, cv=10, refit=True)
+    newTool = model_selection.RandomizedSearchCV(estimator=tool, param_distributions=paramDict, refit=True, n_iter=2, verbose=1)
+    newTool.fit(trainingInput, trainingOutput)
+    end = time.time()
+    print '\t HyperParameter Search done in ' + str(end-start) + 'seconds'
+    print 'best params...'
+    print newTool.best_params_
+    return newTool.best_estimator_
+
 def main():
 
     # get the necessary components to build and use an analysis tool
@@ -63,30 +76,36 @@ def main():
     print( 'scaling...')
     scaler = getStandardScaler(tInput)
     tInput = scaler.transform(tInput)
+    tInput = tInput[-1000:-1]
+    tOutput = tOutput[-1000:-1]
 
     # get a test input and output set
     print( 'creating data subsets..')
-    prelimInput = tInput[0:-5000]
-    prelimOutput = tOutput[0:-5000]
-    testInput = tInput[-5000: -1]
-    testOutput = tOutput[-5000: -1]
+    prelimInput = tInput[0:-500]
+    prelimOutput = tOutput[0:-500]
+    testInput = tInput[-500: -1]
+    testOutput = tOutput[-500: -1]
 
-    # get the tools
+    # get and pre-train each tool
     print( 'getting tools...')
+    print( '\tglm...')
     glmnet = getglmnet()
-    ardRegressor =
-
-
-    # train the models
-    print( 'training glm...')
     glmnet.fit(prelimInput,prelimOutput)
-    print( 'training ard...')
-    ardRegressor.fit(prelimInput, tOutput)
+
+    print '\tard...'
+    ardRegressorDict = {'alpha_1': numpy.arange(1.e-6, 1.e-5, 1.e-6), 'alpha_2': numpy.arange(1.e-6, 1.e-5, 1.e-6)}
+    ardRegressor = fitEstimator(linear_model.ARDRegression(), ardRegressorDict, prelimInput, prelimOutput)
+
 
     # get initial measure of performance
     print( 'R^2 Scores')
-    print( 'glmnet - ' + str(skm.r2_score(testOutput, getTestPredictions(testInput, glmnet))))
-    print( 'glmnet - ' + str(skm.r2_score(testOutput, getTestPredictions(testInput, ardRegressor))))
+    print( 'glmnet - ' + str(metrics.r2_score(y_true=testOutput,y_pred=getTestPredictions(testInput, glmnet, True) )))
+    print( 'ardRegressor - ' + str(metrics.r2_score(y_true=testOutput,y_pred=getTestPredictions(testInput, ardRegressor, False))))
+
+    # # refit all tools with the full data
+    # print 'refitting...'
+    # glmnet.fit(tInput, tOutput)
+    # ardRegressor.fit(tInput, tOutput)
 
 
     # # save the input scaler and the model for later use
